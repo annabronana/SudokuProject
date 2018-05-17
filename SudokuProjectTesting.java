eaa1a6b615c82e3b29aa87061a1b9cf5c650ef7c
schluna <schluna@cf162-15.cs.wwu.edu>
2018-05-17 16:00:45 -0700

    DOES THIS WOOORK???

---
+++ SudokuProjectTesting.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class SudokuProjectTesting {
+
+	public static void main(String[] args) {
+		  ArrayList<Integer> board = new ArrayList<Integer>();
+		  Scanner userInput = new Scanner(System.in);
+		  System.out.print("What number would you like to check? ");
+		  int n = userInput.nextInt();
+		  int L = 0;
+		  for (int i = 0; i <= 9; i++) {
+			  L = L + 1;
+			  board.add(L);
+		  }
+		  for (int j = 0; j <= 9; j ++) {
+			  if (n == board.get(j)) {
+				  System.out.println("That number is indeed in the array!");
+			  }
+
+		  
+		  }  	
+			  
+
+	
+		  }
+	}
+
+
